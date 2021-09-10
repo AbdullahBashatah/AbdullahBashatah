988b9e1fe1748141db1b9ef1629f1fb8a04cf111
Abdullah.Bashatah <bashatah@a483e7397344.ant.amazon.com>
2021-09-10 15:45:32 +1000

    pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 module.exports = {
   reactStrictMode: true,
-  cleanDistDir: false
+  cleanDistDir: false,
+  distDir: 'build',
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      '/about': { page: '/about' },
+      '/p/hello-nextjs': { page: '/post', query: { title: 'hello-nextjs' } },
+      '/p/learn-nextjs': { page: '/post', query: { title: 'learn-nextjs' } },
+      '/p/deploy-nextjs': { page: '/post', query: { title: 'deploy-nextjs' } },
+    }
+  },
 }
